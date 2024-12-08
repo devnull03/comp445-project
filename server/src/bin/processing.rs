c13ba33b024f4e9345692861b4d953f0a1217af4
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-12-07 20:09:09 -0800

    feat: added processing to create sql database

--- server/src/bin/processing.rs
+++ server/src/bin/processing.rs
@@ -1,11 +1,14 @@
 use csv;
 use rand::Rng;
-use std::cmp::min;
-use std::fmt;
-use std::hash::Hash;
+use rusqlite::Connection;
+use serde_json::json;
 use std::{
+    cmp::min,
     collections::{HashMap, HashSet},
+    fmt,
+    fmt::Write,
     fs::File,
+    hash::Hash,
     path::Path,
 };
 
@@ -13,22 +16,28 @@ use std::{
 fn main() {
     let shingle_size = 3;
     let minhash_length = 20;
-    let similarity_threshold = 0.8;
+    let similarity_threshold = 0.85;
     let file_path =
         Path::new("/home/devnull03/school/COMP455/project/server/src/bin/evaluation.csv");
 
-    // ------------------------------------------------------------------------------------------------------------------------------------------
-    // loading the data
+    let mut minhash_data: HashMap<u32, Vec<u64>> = HashMap::new();
+    let mut similarities: HashMap<u32, HashMap<u32, f64>> = HashMap::new();
 
+    // ------------------------------------------------------------------------------------------------------------------------------------------
+    //? loading the data
+
+    println!("Loading file in memory....");
     let records = load_data(file_path);
     let combined_strings: Vec<String> = records
         .values()
-        .map(|record| format!("{} {}", record.title, record.text))
+        .map(|record| record.to_db_string())
         .collect();
-    // let mut inverse_index = build_inverted_index(&records);
+
+    println!("Creating the inverse idex...");
+    let inverse_index: HashMap<String, HashSet<u32>> = build_inverted_index(&records);
 
     // ------------------------------------------------------------------------------------------------------------------------------------------
-    // prepairing the data
+    //? prepairing the data
 
     println!("Creating shingles..");
     let shingled_dataset = create_shingles(&combined_strings, shingle_size);
@@ -37,26 +46,10 @@ fn main() {
     let hash_funcs = generate_hash_funcs(minhash_length);
 
     // ------------------------------------------------------------------------------------------------------------------------------------------
-    // creating stuff needed to store the processed data
-
-    let mut similarities: HashMap<u32, HashMap<u32, f64>> = HashMap::new();
-
-    let similarities_file_path =
-        "/home/devnull03/school/COMP455/project/server/src/bin/similarities.csv";
-    let similarities_file = File::create(similarities_file_path).expect("Failed to create file");
-    let mut writer = csv::Writer::from_writer(similarities_file);
-    writer
-        .write_record(&["Document 1", "Similarities"])
-        .expect("Failed to write header");
-    
-    let mut minhash_data: HashMap<u32, Vec<u64>> = HashMap::new();
-    
-    // ------------------------------------------------------------------------------------------------------------------------------------------
-    // actually processing the data
+    //? actually processing the data
 
     println!("Generating minhashes and compairing...");
     for (doc1_id, doc1) in &shingled_dataset {
-
         let doc1_minhash;
         if let Some(_doc1_minhash) = minhash_data.get(doc1_id).cloned() {
             doc1_minhash = _doc1_minhash;
@@ -67,7 +60,7 @@ fn main() {
 
         let mut doc_similarities: HashMap<u32, f64> = HashMap::new();
 
-        println!("processing {:?}", doc1_id);
+        // println!("processing {:?}", doc1_id);
 
         for (doc2_id, doc2) in &shingled_dataset {
             let doc2_minhash;
@@ -87,26 +80,207 @@ fn main() {
         }
 
         similarities.insert(*doc1_id, doc_similarities);
+    }
+
+    println!("Finished calculating similarities");
+
+    // ------------------------------------------------------------------------------------------------------------------------------------------
+    //? create files for data storage
+
+    println!("Generating the csv file");
+    let similarities_file_path =
+        "/home/devnull03/school/COMP455/project/server/src/bin/similarities.csv";
+    create_csv_file(similarities_file_path, &similarities);
+
+    println!("Generating the sqlite database");
+    create_sqlite_file(
+        "processed.db",
+        &combined_strings,
+        &inverse_index,
+        &similarities,
+    );
+
+    // ------------------------------------------------------------------------------------------------------------------------------------------
+}
+
+pub fn create_sqlite_file(
+    file_name: &str,
+    records: &Vec<String>,
+    inverse_index: &InverseIndexDB,
+    similarities: &HashMap<u32, HashMap<u32, f64>>,
+) {
+    let records_table_headers = vec!["id", "title", "text", "label"];
+    let (records_new_table, records_table_values) =
+        build_table_creation_commands("records", &records_table_headers, &records);
+
+    let inverse_index_headers = vec!["string", "entries"];
+    let inverse_index_string_values: Vec<String> = inverse_index
+        // .values()
+        .into_iter()
+        .map(|v| {
+            format!(
+                r#"("{}", "[{}]")"#,
+                v.0.replace(r#"""#, "”"),
+                format!("{:?}", v.1)
+                    .strip_prefix("{")
+                    .unwrap()
+                    .strip_suffix("}")
+                    .unwrap()
+                    .to_string()
+            )
+        })
+        .collect();
+
+    let (_inverse_index_new_table, inverse_index_table_values) = build_table_creation_commands(
+        "inverse_index",
+        &inverse_index_headers,
+        &inverse_index_string_values,
+    );
+
+    let similarities_headers = vec!["document_id", "similar_documents"];
+    let mut similarities_values: Vec<String> = Vec::new();
+
+    for doc in similarities {
+        let mut val = "[".to_string();
+        let doc_len = doc.1.len();
+        for (idx, ele) in doc.1.into_iter().enumerate() {
+            write!(
+                &mut val,
+                "{}",
+                json!({
+                    "doc_id": ele.0,
+                    "similarity": ele.1
+                })
+                .to_string()
+            )
+            .unwrap();
+            if idx != doc_len {
+                write!(&mut val, ",").unwrap();
+            }
+        }
+        write!(&mut val, "]").unwrap();
+        similarities_values.push(format!(r#"( {}, '{}' )"#, doc.0, val));
+    }
+
+    let (_similarities_new_table, similarities_table_values) =
+        build_table_creation_commands("similarities", &similarities_headers, &similarities_values);
+
+    // db stuff -----------------------------------------------------------------------------------------
+
+    let db_connection = Connection::open(file_name).unwrap();
+
+    db_connection.execute(&records_new_table, ()).unwrap();
+    db_connection
+        .execute(
+            "CREATE TABLE IF NOT EXISTS inverse_index ( string TEXT PRIMARY KEY, entries TEXT )",
+            (),
+        )
+        .unwrap();
+    db_connection.execute("CREATE TABLE IF NOT EXISTS similarities ( document_id INTEGER PRIMARY KEY, similar_documents TEXT )", ()).unwrap();
+
+    let mut counter = 0;
+    for table in vec![
+        &records_table_values,
+        &similarities_table_values,
+        &inverse_index_table_values,
+    ] {
+        println!("inserting values.. table {}", counter);
+        counter += 1;
+        for val in table {
+            if let Err(e) = db_connection.execute(&val, ()) {
+                if let rusqlite::Error::SqliteFailure(ref err, _) = e {
+                    if err.code == rusqlite::ErrorCode::ConstraintViolation {
+                        println!(
+                            "Skipping insertion due to unique constraint violation: {:?}",
+                            e
+                        );
+                        continue;
+                    }
+                }
+                panic!("Failed to execute insertion: {:?}", e);
+            }
+        }
+    }
+
+    // tests --------------------------------------------------------------------------------------------
+}
+
+// TODO: get rid of this or minimize
+pub fn build_table_creation_commands(
+    table_name: &str,
+    table_headers: &Vec<&str>,
+    values: &Vec<String>,
+) -> (String, Vec<String>) {
+    let table_headers_len = table_headers.len();
+
+    let mut table_creation_string = format!("CREATE TABLE IF NOT EXISTS {} (", table_name);
+    let mut insert_statements: Vec<String> = Vec::new();
+    let values_insertion_string = format!("INSERT INTO {} VALUES ", table_name);
+
+    for ele in table_headers.into_iter().enumerate() {
+        if ele.1.contains("id") && ele.0 == 0 {
+            write!(
+                &mut table_creation_string,
+                "{} INTEGER PRIMARY KEY NOT NULL, ",
+                &ele.1
+            )
+            .unwrap();
+            continue;
+        }
+        write!(&mut table_creation_string, "{} TEXT", ele.1).unwrap();
+        if !ele.0.eq(&(table_headers_len - 1)) {
+            write!(&mut table_creation_string, ", ").unwrap();
+        };
+    }
+    write!(&mut table_creation_string, ");").unwrap();
+
+    for val in values {
+        insert_statements.push(format!("{} {};", &values_insertion_string, val));
+    }
+
+    (table_creation_string, insert_statements)
+}
+
+pub fn create_csv_file(
+    similarities_file_path: &str,
+    similarities: &HashMap<u32, HashMap<u32, f64>>,
+) {
+    let similarities_file = File::create(similarities_file_path).expect("Failed to create file");
+    let mut writer = csv::Writer::from_writer(similarities_file);
+    writer
+        .write_record(&["document_id", "similar_documents"])
+        .expect("Failed to write header");
+
+    for (doc1_id, similar_docs) in similarities {
         writer
             .write_record(&[
                 doc1_id.to_string(),
-                fmt::format(format_args!("{:?}", similarities.get(doc1_id).unwrap())),
+                fmt::format(format_args!("{:?}", similar_docs)),
             ])
             .expect("Failed to write record");
     }
 
     writer.flush().expect("Failed to flush writer");
-    println!("Finished calculating similarities");
-
-    // ------------------------------------------------------------------------------------------------------------------------------------------
 }
 
 #[derive(Debug, serde::Deserialize, serde::Serialize, Hash, Clone)]
 pub struct Record {
-    id: u32,
-    title: String,
-    text: String,
-    label: u32, // can be either 1 or 0
+    pub id: u32,
+    pub title: String,
+    pub text: String,
+    pub label: u32, // can be either 1 or 0
+}
+
+impl Record {
+    fn to_db_string(&self) -> String {
+        format!(
+            r#"({}, "{}", "{}", "{}")"#,
+            self.id,
+            self.title.replace(r#"""#, "”"),
+            self.text.replace(r#"""#, "”"),
+            self.label
+        )
+    }
 }
 
 pub type InverseIndexDB = HashMap<String, HashSet<u32>>;
@@ -121,13 +295,16 @@ pub fn load_data(file_path: &Path) -> HashMap<u32, Record> {
         let record: Record = result.expect("Record didn't map correctly");
         final_data.insert(record.id, record);
     }
+    println!("total records: {}", final_data.len());
     final_data
 }
 
-pub fn tokenize(text: &String) -> Vec<String> {
-    let mut tokens: Vec<String> = Vec::new();
+pub fn tokenize(text: &String) -> HashSet<String> {
+    let mut tokens: HashSet<String> = HashSet::new();
     for i in text.split_whitespace() {
-        tokens.push(i.to_lowercase());
+        for e in i.split(".") {
+            tokens.insert(e.to_lowercase());
+        }
     }
     //? would this be better for performance
     //?  text.split_whitespace().map(|x| x.to_lowercase())
@@ -147,7 +324,7 @@ pub fn build_inverted_index(records: &HashMap<u32, Record>) -> InverseIndexDB {
             if inverted_index.contains_key(&token) {
                 inverted_index.get_mut(&token).unwrap().insert(record.id);
             } else {
-                println!("Created key {:?}", &token);
+                // println!("Created key {:?}", &token);
                 inverted_index.insert(token, HashSet::from([record.id]));
             }
         }
@@ -214,14 +391,14 @@ fn generate_minhash_signature(
     minhash_signature
 }
 
-/// Calculate the Jaccard similarity between two sets.
+// Calculate the Jaccard similarity between two sets.
 pub fn jaccard(a: &HashSet<String>, b: &HashSet<String>) -> f64 {
     let intersection: HashSet<_> = a.intersection(b).collect();
     let union: HashSet<_> = a.union(b).collect();
     intersection.len() as f64 / union.len() as f64
 }
 
-/// Calculate the similarity between two MinHash signatures.
+// Calculate the similarity between two MinHash signatures.
 pub fn minhash_similarity(a: &Vec<u64>, b: &Vec<u64>) -> f64 {
     let matches = a.iter().zip(b).filter(|&(x, y)| x == y).count();
     matches as f64 / a.len() as f64
